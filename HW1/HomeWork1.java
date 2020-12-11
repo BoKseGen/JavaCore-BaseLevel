d896bda6c6a84f69afd61a2376183ba54788a43c
Ksenia <ksenshaaa@gmail.com>
2020-12-11 04:11:17 +0300

    upd HomeWork1, check variable d

--- HW1/HomeWork1.java
+++ HW1/HomeWork1.java
@@ -5,17 +5,29 @@ public class HomeWork1 {
     /* Method for 1st task */
     public static float countExpression(int a, int b, int c, int d) {
 
-        return a * (b + ((float)c / d));
+        if (d != 0)
+            return a * (b + ((float)c / d));
+        else {
+            System.out.println("А на 0 делить-то нельзя...\n" +
+                    "Поменяйте значение d с 0 на любое другое.\n" +
+                    " А пока вместо ответа выведем Вам -1.0");
+            return -1;
+        }
 
     }
-    
+
     public static void main(String[] args){
 
         int a = 16;
         int b = 30;
         int c = 1100;
         int d = 45;
+        System.out.println(countExpression(a, b, c, d));
 
+        a = 16;
+        b = 30;
+        c = 1100;
+        d = 0;
         System.out.println(countExpression(a, b, c, d));
 
     }
